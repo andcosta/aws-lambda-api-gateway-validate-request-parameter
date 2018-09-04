8d34249aa42e5e8161165240cb0d2e54e5fe87fa
Anderson de Carvalho <anderson.analistati@gmail.com>
2018-09-03 21:29:01 -0300

    Unit test - Parameter value

--- __tests__/parameter.test.js
+++ __tests__/parameter.test.js
@@ -3,5 +3,14 @@ import RequestParameter from '../src';
 describe('Request Parameter', () => {
   test('Parameter is empty', async () => {});
 
-  test('Parameter value', async () => {});
+  test('Parameter value', async () => {
+    const mock_request_parameter = {
+      param1: 'value1',
+      param2: 'value2',
+      param3: 'value3'
+    };
+
+    const value_param1 = RequestParameter.value(mock_request_parameter, 'param1');
+    expect(value_param1).toEqual(mock_request_parameter.param1);
+  });
 });
